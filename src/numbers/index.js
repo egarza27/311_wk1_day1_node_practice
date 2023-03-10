591a5d128a9cb273bf9072fee8efa854f7bdaf69
egarza27 <elizabethigarza10@gmail.com>
2023-03-09 20:07:36 -0600

    upload final project files

--- src/numbers/index.js
+++ src/numbers/index.js
@@ -1,20 +1,25 @@
 const isEven = (num) => {
   // write code for numbers.isEven
+  if (num % 2 === 0) {
+    return true;
+  }
+};
 
-}
-
-const sum = (arr) => {
-  // write code for numbers.sum
-
-}
+const sum = (arr) => arr.reduce((a, b) => a + b);
 
 const comboSum = (arr, sum) => {
   // write code for numbers.comboSum
-
-}
+  for (let i = 0; i < arr.length; i++) {
+    for (let n = -1; n < arr.length; n++)
+      if (arr[i] + arr[n] === sum) {
+        return true;
+      }
+  }
+  return false;
+};
 
 module.exports = {
   isEven,
   sum,
-  comboSum
-}
+  comboSum,
+};
--- src/strings/index.js
+++ src/strings/index.js
@@ -1,20 +1,25 @@
-const split = (str, delim) => {
-  // write code for strings.split
-
-}
+const split = (str, delim) => str.split(delim);
 
 const pairs = (str) => {
-  // write code for strings.pairs
+  let splitStringArr = [];
+  for (let i = 0; i < str.length; i = i + 2) {
+    splitStringArr.push(str.slice(i, i + 2));
+  }
+  return splitStringArr;
+};
 
-}
+console.log(pairs("hello"));
 
 const reverse = (str) => {
   // write code for strings.reverse
-
-}
+  const stringArray = str.split("");
+  const reverseArray = stringArray.reverse();
+  const arrayToString = reverseArray.join("");
+  return arrayToString;
+};
 
 module.exports = {
   split,
   pairs,
-  reverse
-}
+  reverse,
+};
